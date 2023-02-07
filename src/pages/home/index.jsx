f9507f570e20b7f51da32e5f98f501ef049499f6
Hardi <hardi.syafrizal@indivaragroup.com>
2023-02-07 11:08:15 +0700

    Feat: big update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,7 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import SearchIcon from "@mui/icons-material/Search";
 import {
   Box,
+  Button,
   Card,
   CardContent,
   CardMedia,
@@ -16,11 +17,7 @@ import {
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import {
-  DISCOVER_API,
-  IMAGE_PATH,
-  SEARCH_API
-} from "../../services/api";
+import { DISCOVER_API, IMAGE_PATH, SEARCH_API } from "../../services/api";
 import "./index.css";
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -81,103 +78,122 @@ const Home = () => {
           </button>
         </form>
       </header>
-
-      <Typography gutterBottom variant="button" component="div" align="right">
-        Page: {pageApi}
-      </Typography>
-
-      <Box mt={2} mb={5} className="container">
-        {movies.map((movie) => (
-          <Card
-            sx={{
-              maxWidth: 345,
-              borderTopRightRadius: "25px",
-              borderTopLeftRadius: "25px",
-            }}
-            className="grid-item"
-            key={movie.id}
+      {movies.length ? (
+        <>
+          <Typography
+            gutterBottom
+            variant="caption"
+            component="div"
+            align="right"
           >
-            {loading && (
-              <Skeleton variant="rectangular" width={500} height={300} />
-            )}
-            {!loading && (
-              <CardMedia
-                component="img"
-                alt={movie.title}
-                image={IMAGE_PATH + movie.poster_path}
-                onError={({ currentTarget }) => {
-                  currentTarget.onerror = null;
-                  currentTarget.src = `https://picsum.photos/200/300?${Math.random()}`;
-                }}
-              />
-            )}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {loading === false ? (
-                  movie.title
-                ) : (
-                  <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
-                )}
-              </Typography>
-              <Typography
-                align="justify"
-                variant="body2"
-                gutterBottom
-                color="text.secondary"
+            Page: {pageApi}
+          </Typography>
+          <Box mt={2} mb={5} className="container">
+            {movies.map((movie) => (
+              <Card
                 sx={{
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  display: "-webkit-box",
-                  WebkitLineClamp: "3",
-                  WebkitBoxOrient: "vertical",
+                  maxWidth: 345,
+                  borderTopRightRadius: "25px",
+                  borderTopLeftRadius: "25px",
                 }}
+                className="grid-item"
+                key={movie.id}
               >
-                {loading === false ? (
-                  movie.overview
-                ) : (
-                  <>
-                    <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
-                    <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
-                  </>
+                {loading && (
+                  <Skeleton variant="rectangular" width={500} height={300} />
                 )}
-              </Typography>
-              <Typography align="left" variant="subtitle1">
-                Release: {movie.release_date}
-              </Typography>
-            </CardContent>
-            {!loading && movie.vote_average ? (
-              <span className={"movie-voting"}>{movie.vote_average}</span>
-            ) : null}
-            {loading && (
-              <Skeleton
-                className="movie-voting"
-                variant="circular"
-                width={16}
-                height={16}
-              />
-            )}
-          </Card>
-        ))}
-      </Box>
+                {!loading && (
+                  <CardMedia
+                    component="img"
+                    alt={movie.title}
+                    image={IMAGE_PATH + movie.poster_path}
+                    onError={({ currentTarget }) => {
+                      currentTarget.onerror = null;
+                      currentTarget.src = `https://picsum.photos/200/300?${Math.random()}`;
+                    }}
+                  />
+                )}
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {loading === false ? (
+                      movie.title
+                    ) : (
+                      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+                    )}
+                  </Typography>
+                  <Typography
+                    align="justify"
+                    variant="body2"
+                    gutterBottom
+                    color="text.secondary"
+                    sx={{
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      display: "-webkit-box",
+                      WebkitLineClamp: "3",
+                      WebkitBoxOrient: "vertical",
+                    }}
+                  >
+                    {loading === false ? (
+                      movie.overview
+                    ) : (
+                      <>
+                        <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+                        <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+                      </>
+                    )}
+                  </Typography>
+                  <Typography align="left" variant="subtitle1">
+                    Release: {movie.release_date}
+                  </Typography>
+                </CardContent>
+                {!loading && movie.vote_average ? (
+                  <span className={"movie-voting"}>{movie.vote_average}</span>
+                ) : null}
+                {loading && (
+                  <Skeleton
+                    className="movie-voting"
+                    variant="circular"
+                    width={16}
+                    height={16}
+                  />
+                )}
+              </Card>
+            ))}
+          </Box>
 
-      <Stack spacing={2} alignItems="center">
-        <Pagination
-          color="primary"
-          variant="outlined"
-          shape="rounded"
-          showFirstButton
-          showLastButton
-          boundaryCount={2}
-          count={totalPage}
-          onChange={(e, value) => setPageApi(value)}
-          renderItem={(item) => (
-            <PaginationItem
-              slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-              {...item}
+          <Stack spacing={2} alignItems="center">
+            <Pagination
+              color="info"
+              shape="rounded"
+              showFirstButton
+              showLastButton
+              siblingCount={4}
+              // boundaryCount={1}
+              size="large"
+              count={totalPage}
+              onChange={(e, value) => {
+                if (e) e.preventDefault();
+                window.scrollTo(0, 0);
+                setPageApi(value);
+              }}
+              renderItem={(item) => (
+                <PaginationItem
+                  slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                  {...item}
+                />
+              )}
             />
-          )}
-        />
-      </Stack>
+          </Stack>
+        </>
+      ) : (
+        <>
+          <Typography variant="h4">Movie Not Available</Typography>
+          <Button variant="contained" startIcon={<ArrowBackIcon />} href="/">
+            Back to Home
+          </Button>
+        </>
+      )}
     </>
   );
 };
